741a2b643202166bbc2bff2bcb3e1d143e3602c0
yashrajmaurya-inventyv <yashraj.maurya.inventyv@gmail.com>
2026-01-05 18:01:48 +0530

    Part2 of Day1

---
+++ Day 1/Part 2.js
@@ -0,0 +1,37 @@
+import { readFile } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const inputPath = path.join(__dirname, 'Input.txt');
+
+readFile(inputPath, 'utf-8', (err, data) => {
+    if (err) {
+        console.error('Error reading file:', err.message);
+        return;
+    }
+
+    let strt = 50;
+    let count = 0;
+    const lines = data.trim().split(/\r?\n/).filter(Boolean);
+    
+    lines.forEach((item)=>{
+       const match = item.toString().trim().match(/^([LR])\s*(\d+)$/i);
+        if (!match) return; 
+        const dir = match[1].toUpperCase();
+        const val = Number(match[2]);
+
+        for(let i=0; i<val; i++){
+            if(dir === 'L'){
+                strt = ((strt -1) + 100) % 100
+            }else if(dir === 'R'){
+                strt = ((strt + 1) -100)%100
+            }
+            if (strt === 0) count++;
+        }
+
+    })
+    console.log(count);
+    
+});
